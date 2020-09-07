604469418509320aeec965bdddb1d1429b350343
DenisKostyukov <68915197+DenisKostyukov@users.noreply.github.com>
2020-09-07 12:38:19 +0300

    com

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ $(function () {
   close.on('click', function (event) {
     var block_position = $('.about').offset().top - 2;
     var position = $(document).scrollTop();
-    if (position < block_position) { // если позиция скролла страницы больше, то ставим фикс
+    if (position < block_position) {
     } else {
       $('.header').addClass('fixed');
     }
